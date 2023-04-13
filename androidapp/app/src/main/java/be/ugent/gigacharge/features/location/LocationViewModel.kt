a1a35d87ee8d118e2017915aef24458af6b461e5
JarneClauw <jarne@clauw.eu>
2023-04-13 14:44:03 +0200

    Locations and location van firestore gebruiken

--- androidapp/app/src/main/java/be/ugent/gigacharge/features/location/LocationViewModel.kt
+++ androidapp/app/src/main/java/be/ugent/gigacharge/features/location/LocationViewModel.kt
@@ -2,58 +2,56 @@ package be.ugent.gigacharge.features.location
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import be.ugent.gigacharge.domain.GetLocationsUseCase
 import be.ugent.gigacharge.model.location.Location
 import be.ugent.gigacharge.model.location.QueueState
 //import be.ugent.gigacharge.data.local.models.Location
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LocationViewModel @Inject constructor(
-    // USECASES
+    getLocationsUseCase: GetLocationsUseCase
 ): ViewModel() {
-    private var location: MutableStateFlow<Location> = MutableStateFlow(Location("", "Roularta Rouselare", QueueState.NotJoined, 0))
+    // private var location: MutableStateFlow<Location> = MutableStateFlow(Location("", "Roularta Rouselare", QueueState.NotJoined, 0))
     // Jarne versie:
     //private var location: MutableStateFlow<Location> = MutableStateFlow(Location("Roularta Roeselare", true))
-    val locationUiState: StateFlow<LocationUiState> = location.map{LocationUiState.Success(it)}.stateIn(viewModelScope, SharingStarted.Eagerly, LocationUiState.Loading)
+    //val locationUiState: StateFlow<LocationUiState> = location.map{LocationUiState.Success(it)}.stateIn(viewModelScope, SharingStarted.Eagerly, LocationUiState.Loading)
 
-    private var locations: MutableStateFlow<List<Location>> = MutableStateFlow(listOf(
-        Location("", "UGent", QueueState.NotJoined, 0),
-        Location("", "UZ Gent", QueueState.NotJoined, 0)
-        // Jarne versie:
-        //Location("UGent", false),
-        //Location("UZ Gent", true)
-    ))
-    val locationsUiState: StateFlow<LocationsUiState> = locations.map{LocationsUiState.Success(it)}.stateIn(viewModelScope, SharingStarted.Eagerly, LocationsUiState.Loading)
+    val locationsUiState: StateFlow<LocationsUiState> = getLocationsUseCase().map{LocationsUiState.Success(it)}.stateIn(viewModelScope, SharingStarted.Eagerly, LocationsUiState.Loading)
+    val locationUiState: StateFlow<LocationUiState> = getLocationsUseCase().map{
+            if (it.getOrNull(0) == null) LocationUiState.Loading else LocationUiState.Success(it[0])
+        }.stateIn(viewModelScope, SharingStarted.Eagerly, LocationUiState.Loading)
 
     fun setLocation(loc: Location) {
-        val temp: MutableList<Location> = locations.value.toMutableList()
-        temp.add(location.value)
-        temp.remove(loc)
-        locations.value = temp.toList()
-        location.value = loc
+//        val temp: MutableList<Location> = locations.value.toMutableList()
+//        temp.add(location.value)
+//        temp.remove(loc)
+//        locations.value = temp.toList()
+//        location.value = loc
     }
 
     fun toggleFavorite(loc: Location) {
-        // Toggle if it was the selected location
-        if (location.value.name == loc.name) {
-            location.value = Location("", location.value.name, QueueState.NotJoined, 0)
-            //Jarne versie:
-            //location.value = Location(location.value.name, !location.value.favorite)
-        }
-        // Toggle a location in the list
-        val temp: MutableList<Location> = mutableListOf()
-        locations.value.forEach { l: Location ->
-            if (l.name == loc.name) {
-                temp.add(Location("", l.name, QueueState.NotJoined, 0))
-                // Jarne versie:
-                //temp.add(Location(l.name, !l.favorite))
-            } else {
-                temp.add(l)
-            }
-        }
-        locations.value = temp
+//        // Toggle if it was the selected location
+//        if (location.value.name == loc.name) {
+//            location.value = Location("", location.value.name, QueueState.NotJoined, 0)
+//            //Jarne versie:
+//            //location.value = Location(location.value.name, !location.value.favorite)
+//        }
+//        // Toggle a location in the list
+//        val temp: MutableList<Location> = mutableListOf()
+//        locations.value.forEach { l: Location ->
+//            if (l.name == loc.name) {
+//                temp.add(Location("", l.name, QueueState.NotJoined, 0))
+//                // Jarne versie:
+//                //temp.add(Location(l.name, !l.favorite))
+//            } else {
+//                temp.add(l)
+//            }
+//        }
+//        locations.value = temp
     }
 }
