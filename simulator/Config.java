4766f9d10b59c48e7c898cd1de3d8395f2a544ea
Robin Paret <robin.paret@ugent.be>
2023-04-17 22:42:47 +0200

    niet alle files geselecteerd eops

--- simulator/Config.java
+++ simulator/Config.java
@@ -5,5 +5,5 @@ public class Config {
     static String endpoint = "http://localhost:3000";
     static String greenfootinstall = "";
 
-    static List<String> userids = List.of("PiPAKLMr9ggNgTvrLUtQ5GaLGOi1", "RAifgUT5LygtlwuOs0B5LVR6Mrf2", "fn3LOXBSnLctpx4fGIGb57pJxPL2");
+    static List<String> userids = List.of("PiPAKLMr9ggNgTvrLUtQ5GaLGOi1", "RAifgUT5LygtlwuOs0B5LVR6Mrf2", "1fUHKEdHf1atn0UrS8THnqmWQ8i1");
 }
